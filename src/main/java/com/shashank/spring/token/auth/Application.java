1684882205035bc050935066afa97d03d72bbe4f
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-07 11:36:14 +0530

    initial commit with dependency

---
+++ src/main/java/com/shashank/spring/token/auth/Application.java
@@ -0,0 +1,13 @@
+package com.shashank.spring.token.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
