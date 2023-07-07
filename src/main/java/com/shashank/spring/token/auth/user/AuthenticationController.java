a1c3eecf2400bcd6d5df75dde938c4dc23b2a4d3
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-07 16:25:18 +0530

    Add. security config

--- src/main/java/com/shashank/spring/token/auth/user/AuthenticationController.java
+++ src/main/java/com/shashank/spring/token/auth/user/AuthenticationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.shashank.spring.token.auth.user.model.LoginRequest;
 import com.shashank.spring.token.auth.user.model.User;
 
 @RestController
@@ -17,7 +16,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<String> login(@RequestBody User loginUser) {
         return ResponseEntity.ok("Login");
     }
 }
