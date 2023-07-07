500876ef6cd760dd515985d7440a66e6088ef5f8
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-07-07 13:59:44 +0530

    Add. endpoint signup and login

---
+++ src/main/java/com/shashank/spring/token/auth/user/AuthenticationController.java
@@ -0,0 +1,23 @@
+package com.shashank.spring.token.auth.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shashank.spring.token.auth.user.model.LoginRequest;
+import com.shashank.spring.token.auth.user.model.User;
+
+@RestController
+public class AuthenticationController {
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody User user) {
+        return ResponseEntity.ok("signup");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok("Login");
+    }
+}
